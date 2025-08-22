061b323470da14078196c6fea18aabdc5b6dc606
Nyoman Sunima <nyomansunima@gmail.com>
2025-08-22 22:32:01 +0800

    refactor: remove footer

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -2,7 +2,6 @@ export * from "./ui/accordion"
 export * from "./ui/button"
 
 export * from "./content"
-export * from "./footer"
 export * from "./header"
 export * from "./navigation-link"
 export * from "./section-label"
