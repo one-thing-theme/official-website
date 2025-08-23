3ab807ba4dfd667f7b30b3a2685f6adc4af9c73a
Nyoman Sunima <nyomansunima@gmail.com>
2025-08-23 23:04:02 +0800

    refactor: update the class, design

--- src/features/changelog/changelog-item.tsx
+++ src/features/changelog/changelog-item.tsx
@@ -1,10 +1,4 @@
-"use client"
-
 import { Link } from "react-router"
-import * as React from "react"
-import { motion } from "motion/react"
-
-const MotionLink = motion.create(Link)
 
 export interface ChangelogData {
   title: string
@@ -18,35 +12,21 @@ interface ChangelogItemProps {
   data: ChangelogData
 }
 
-export function ChangelogItem({
-  data,
-}: ChangelogItemProps): React.ReactElement {
+export function ChangelogItem({ data }: ChangelogItemProps) {
   const { title, date, image, description, url } = data
 
   return (
-    <MotionLink
-      initial={{ opacity: 0, y: 200, scale: 0.98 }}
-      whileInView={{ opacity: 1, y: 0, scale: 1 }}
-      transition={{ type: "spring", duration: 1.2, delay: 0.2 }}
-      viewport={{ once: true, margin: "-20% 0px" }}
-      to={url}
-      target="_blank"
-      className="flex flex-col gap-4 bg-surface border border-border rounded-2xl p-4 group"
-    >
-      <div className="flex flex-col tablet:flex-row gap-2 tablet:items-center justify-between">
-        <span className="font-medium">{title}</span>
-        <span className="text-sm text-foreground/50 group-hover:text-foreground">
-          {date}
-        </span>
+    <Link to={url} target="_blank" className="flex flex-col">
+      <div className="flex items-center gap-4">
+        <h3 className="font-medium">{title}</h3>
+        <span className="text-sm text-foreground/40">{date}</span>
       </div>
 
-      <picture className="relative w-full h-[180px] tablet:h-[312px] overflow-hidden rounded-xl border border-border">
-        <img src={image} alt={title} className="object-cover" />
-      </picture>
+      <p className="leading-7 text-pretty mt-3">{description}</p>
 
-      <p className="text-sm text-foreground/80 leading-relaxed!">
-        {description}
-      </p>
-    </MotionLink>
+      <div className="relative w-full h-[180px] tablet:h-[312px] overflow-hidden rounded-xl mt-6">
+        <img src={image} alt={title} className="object-cover" />
+      </div>
+    </Link>
   )
 }
--- src/features/changelog/changelog-section.tsx
+++ src/features/changelog/changelog-section.tsx
@@ -1,25 +1,22 @@
 import { SectionLabel } from "@shared/components"
-import * as React from "react"
 import { ChangelogList } from "./changelog-list"
 
-export function ChangelogSection(): React.ReactElement {
+export function ChangelogSection() {
   return (
-    <section className="flex py-36 container mx-auto px-5 tablet:px-0">
-      <div className="flex flex-col items-center mx-auto w-full">
-        <SectionLabel>What’s new</SectionLabel>
+    <section className="flex flex-col items-center py-28 container mx-auto px-5 tablet:px-0">
+      <SectionLabel>What’s new</SectionLabel>
 
-        <h2 className="text-xl text-foreground font-medium leading-tight! text-balance text-center mt-8 tablet:w-7/12">
-          Changelog
-        </h2>
+      <h2 className="text-xl text-foreground font-medium leading-tight! text-balance text-center mt-8 tablet:w-7/12">
+        Changelog
+      </h2>
 
-        <p className="leading-relaxed! tablet:w-5/12 text-center text-foreground/60 mt-6 text-pretty">
-          New updates & improvements, including the bug fixes and features
-          releases. You can find all of the versions with the release notes
-        </p>
+      <p className="leading-relaxed! tablet:w-5/12 text-center text-foreground/60 mt-6 text-pretty">
+        New updates & improvements, including the bug fixes and features
+        releases. You can find all of the versions with the release notes
+      </p>
 
-        <div className="flex items-center mt-16 w-full tablet:w-6/12 tablet:px-10">
-          <ChangelogList />
-        </div>
+      <div className="flex items-center mt-16 w-full tablet:w-6/12 tablet:px-10">
+        <ChangelogList />
       </div>
     </section>
   )
