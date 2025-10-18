0ac53daf1787ab1ca63a1c6a34eb9a5dbf7bc382
Nyoman Sunima <nyomansunima@gmail.com>
2025-10-18 23:39:20 +0800

    refactor: move to tanstack

--- src/app/routes/changelog.tsx
+++ src/app/routes/changelog.tsx
@@ -1,18 +1,22 @@
-import { ChangelogSection } from "@features/changelog"
-import { generatedMetadata } from "@shared/libs"
+import { ChangelogSection } from "@features/changelog";
+import { generatedMetadata } from "@shared/libs";
+import { createFileRoute } from "@tanstack/react-router";
 
-export function meta() {
-  return generatedMetadata({
-    title: "What's new | One thing theme",
-    description:
-      "See what's new in the progress, we always ship and fix in a weeks",
-  })
-}
+export const Route = createFileRoute("/changelog")({
+  head: () => ({
+    meta: generatedMetadata({
+      title: "What's new | One thing theme",
+      description:
+        "See what's new in the progress, we always ship and fix in a weeks",
+    }),
+  }),
+  component: PageComponent,
+});
 
-export default function ChangelogPage() {
+function PageComponent() {
   return (
     <main className="flex flex-col">
       <ChangelogSection />
     </main>
-  )
+  );
 }
--- src/app/routes/docs.$.tsx
+++ src/app/routes/docs.$.tsx
@@ -3,62 +3,64 @@ import {
   DocsContent,
   getBreadcrumb,
   getDocDetail,
-  getToc,
   Navigation,
-  TableOfContent,
-} from "@features/docs"
-import { generatedMetadata } from "@shared/libs"
-import type { Route } from "./+types/docs.$"
+} from "@features/docs";
+import { generatedMetadata } from "@shared/libs";
+import { createFileRoute } from "@tanstack/react-router";
 
-export async function loader({ params }) {
-  const slug = params["*"]
+// generatedMetadata({
+//     title: `Docs - ${doc.detail.data.title} | One thing theme`,
+//     description: `${doc.detail.data.description}`,
+//   })
 
-  const bc = await getBreadcrumb(slug)
-  const detail = await getDocDetail(slug)
-  const toc = await getToc(slug)
+export const Route = createFileRoute("/docs/$")({
+  head: () => ({
+    meta: generatedMetadata({
+      title: "Minimal code theme | One Thing Theme",
+      description:
+        "The best clean code editor theme that combine simplicity and clean look",
+    }),
+  }),
+  loader: async (ctx) => {
+    const slug = ctx.params._splat || "";
 
-  return {
-    doc: {
-      bc,
-      detail,
-      toc,
-    },
-  }
-}
+    const bc = await getBreadcrumb(slug);
+    const detail = await getDocDetail(slug);
 
-export function meta({ data }) {
-  const { doc } = data
+    return {
+      doc: {
+        bc,
+        detail,
+      },
+    } as any;
+  },
+  component: PageComponent,
+});
 
-  return generatedMetadata({
-    title: `Docs - ${doc.detail.data.title} | One thing theme`,
-    description: `${doc.detail.data.description}`,
-  })
-}
-
-export default async function DocsPage({ loaderData }: Route.ComponentProps) {
-  const { doc } = loaderData
+function PageComponent() {
+  const { doc } = Route.useLoaderData();
 
   return (
     <div className="flex flex-col">
       <div className="flex">
         {/* left */}
-        <aside className="hidden laptop:flex flex-col h-full w-[460px] p-3 border-border sticky top-28 overflow-y-auto">
+        <aside className="sticky top-28 laptop:flex hidden h-full w-[460px] flex-col overflow-y-auto border-border p-3">
           <Navigation />
         </aside>
 
         {/* center  */}
-        <div className="flex container mx-auto pt-28">
-          <div className="flex flex-col w-9/12 mx-auto gap-6 pb-28 tablet:pb-36">
+        <div className="container mx-auto flex pt-28">
+          <div className="mx-auto flex w-9/12 flex-col gap-6 pb-28 tablet:pb-36">
             <Breadcrumb breadscrumbs={doc.bc} />
             <DocsContent doc={doc.detail} />
           </div>
         </div>
 
         {/* right */}
-        <aside className="hidden laptop:flex flex-col h-full w-[480px] p-3 sticky top-28 overflow-y-auto">
-          <TableOfContent toc={doc.toc} />
+        <aside className="sticky top-28 laptop:flex hidden h-full w-[480px] flex-col overflow-y-auto p-3">
+          {/*<TableOfContent toc={doc.toc} />*/}
         </aside>
       </div>
     </div>
-  )
+  );
 }
